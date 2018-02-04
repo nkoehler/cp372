2dd3cf5ef456870011f72e672f765cd024727cab
GrahamDubreuil <31933238+GrahamDubreuil@users.noreply.github.com>
2018-02-03 19:09:41 -0500

    added revision / fixed message.validate (.equals() is a fuck)

--- Client/Message.java
+++ Client/Message.java
@@ -19,14 +19,15 @@ public class Message {
 				for(int i=0; i<13;i++) {
 					x[i] = Character.getNumericValue(ISBN.charAt(i));
 				}
-				int s = x[1] + (3*x[2]) + x[3] + (3*x[4]) + x[5] + (3*x[6]) + x[7] + (3*x[8]) + x[9] + (3*x[10]) + x[11] + (3*x[12]);
-				s = 10 - (s%10);
-				if(s == x[13]) {
+				int s = x[0] + (3*x[1]) + x[2] + (3*x[3]) + x[4] + (3*x[5]) + x[6] + (3*x[7]) + x[8] + (3*x[9]) + x[10] + (3*x[11]);
+				s = 10 -(s%10);
+				if(s == x[12]) {
 					return true;
 				}
 			}catch(Exception e) {
 				return false;
 			}
+			
 		}
 		return false;
 	}
@@ -39,18 +40,18 @@ public class Message {
 		}
 	}
 	boolean validate() {
-		/*if(this.type == "SUBMIT"||this.type == "UPDATE") {
+		if(this.type.equals("SUBMIT")||this.type.equals("UPDATE")) {
 			if(ISBN_check(this.ISBN)) {
-				if(this.year != "") {
+				if(!this.year.equals("")) {
 					return year_check(this.year);
 				}else {
 					return true;
 				}
 			}
-		}else if(this.type == "REMOVE"||this.type == "GET") {
-			if(this.ISBN != "") {
+		}else if(this.type.equals("REMOVE")||this.type.equals("GET")) {
+			if(!this.ISBN.equals("")) {
 				if(ISBN_check(this.ISBN)) {
-					if(this.year != "") {
+					if(!this.year.equals("")) {
 						return year_check(this.year);
 					}else {
 						return true;
@@ -58,8 +59,7 @@ public class Message {
 				}
 			}
 		}
-		return false;*/
-		return true;
+		return false;
 	}
 	public String toString() {
 		this.message = this.type;
